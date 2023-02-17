cbbdc59ccb475945e1af358ce6ffddad0b7c63fc
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-02-17 23:01:46 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const connection = mongoose.connection;
+connection.once("open", ()=>{
+    console.log("MongoDB Connection Success!");
+});
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is up and running on PORT : ${PORT}`);
+});
