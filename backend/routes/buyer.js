c816069506696b1873143f2ac85b59676a98bcf6
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-02-18 13:54:11 +0530

    Added get buyer by email route

--- backend/routes/buyer.js
+++ backend/routes/buyer.js
@@ -55,4 +55,14 @@ router.route("/get/:id").get(async(req, res)=>{
     })
 })
 
+router.route("/get/email/:email").get(async(req, res)=>{
+    let email = req.params.email;
+    await Buyer.find({"email": `${email}`}).then((buyer)=>{
+        res.json(buyer);
+    }).catch((err)=>{
+        console.log(err.message);
+        res.status(500).send({status: "Error with get the buyer", error: err.message});
+    })
+})
+
 module.exports = router;
