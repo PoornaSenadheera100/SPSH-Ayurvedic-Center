5ff3fb16d1ad601140c1e71be85b2d39fab0b1c3
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-02-18 11:35:51 +0530

    Added delete and get buyer routes

--- backend/routes/buyer.js
+++ backend/routes/buyer.js
@@ -34,4 +34,25 @@ router.route("/").get((req, res)=>{
     })
 })
 
+router.route("/delete/:id").delete(async(req, res)=>{
+    let buyerId = req.params.id;
+
+    await Buyer.findByIdAndDelete(buyerId).then(()=>{
+        res.status(200).send({status: "Buyer Deleted"});
+    }).catch((err)=>{
+        console.log(err.message);
+        res.status(500).send({status: "Error with delete Buyer", error: err.message});
+    })
+})
+
+router.route("/get/:id").get(async(req, res)=>{
+    let buyerId = req.params.id;
+    const buyer = await Buyer.findById(buyerId).then((buyer)=>{
+        res.status(200).send({status: "Buyer fetched", buyer})
+    }).catch((err)=>{
+        console.log(err.message);
+        res.status(500).send({status: "Error with get buyer", error: err.message});
+    })
+})
+
 module.exports = router;
