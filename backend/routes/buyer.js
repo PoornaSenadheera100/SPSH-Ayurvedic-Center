19b7d0869c5967445e8213026de831b705bbedeb
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-02-18 10:35:30 +0530

    Created buyer router file

---
+++ backend/routes/buyer.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+let Buyer = require("../models/Buyer");
+
+router.route("/add").post((req, res)=>{
+    const name = req.body.name;
+    const address = req.body.address;
+    const nic = req.body.nic;
+    const email = req.body.email;
+    const phone = req.body.phone;
+    const password = req.body.password;
+
+
+    const newBuyer = new Buyer({
+        name,
+        address,
+        nic,
+        email,
+        phone,
+        password
+    })
+
+    newBuyer.save().then(()=>{
+        res.json("Buyer Added.");
+    }).catch((err)=>{
+        console.log(err);
+    })
+})
+
+module.exports = router;
