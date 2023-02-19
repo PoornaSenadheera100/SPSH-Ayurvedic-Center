79ae205ef717c120978cda8c8fbae3a23e804ff7
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-02-19 08:34:22 +0530

    Linked to Buyer, Seller, Admin login components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import { BrowserRouter as Router, Route, } from "react-router-dom";
 // import Header from './components/Header';
 import Welcome from './components/Welcome';
+import LoginBuyer from './components/LoginBuyer';
+import LoginSeller from './components/LoginSeller';
+import LoginAdmin from './components/LoginAdmin';
 
 function App() {
   return (
     <Router>
       {/* <Header/> */}
       <Route path = '/' exact component = {Welcome}/>
+      <Route path = '/buyerlogin' exact component = {LoginBuyer}/>
+      <Route path = '/sellerlogin' exact component = {LoginSeller}/>
+      <Route path = '/adminlogin' exact component = {LoginAdmin}/>
     </Router>
   );
 }
