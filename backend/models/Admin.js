d92ba0a3169b738dea5c15ee3f038c0628155f89
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-02-19 09:01:22 +0530

    Created Admin model

---
+++ backend/models/Admin.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const adminSchema = new Schema({
+    email : {
+        type : String,
+        required : true
+    },
+
+    password : {
+        type : String,
+        required : true
+    }
+})
+
+const Admin = mongoose.model("Admin", adminSchema);
+
+module.exports = Admin;
