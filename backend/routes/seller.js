778e149bf7c712736e6f998e4e3caa39f0be51a0
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-02-19 22:26:42 +0530

    Created seller router with adding function

---
+++ backend/routes/seller.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+let Seller = require("../models/Seller");
+
+router.route("/add").post((req, res)=>{
+    const name = req.body.name;
+    const email = req.body.email;
+    const phone = req.body.phone;
+    const password = req.body.password;
+
+
+    const newSeller = new Seller({
+        name,
+        email,
+        phone,
+        password
+    })
+
+    newSeller.save().then(()=>{
+        res.json("Seller Added.");
+    }).catch((err)=>{
+        console.log(err);
+    })
+})
+
+module.exports = router;
