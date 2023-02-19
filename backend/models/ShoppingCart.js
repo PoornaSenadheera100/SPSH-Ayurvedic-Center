7ca2c7609c81d9d38f870ca875e88fbc263a6ff9
Sathira-Wijeratne <swije27@gmail.com>
2023-02-19 16:54:31 +0530

    added some attributes to cartScheme

--- backend/models/ShoppingCart.js
+++ backend/models/ShoppingCart.js
@@ -3,5 +3,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const cartShema = new Schema({
-    
+    buyerID : {
+
+    },
+    itemID : {
+        type : Array,
+        
+    },
+
 })
