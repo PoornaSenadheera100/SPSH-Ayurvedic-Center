e5f738460505317ff162777632a95faab39eea2e
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-02-19 08:20:30 +0530

    Added Welcome component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router, Route, } from "react-router-dom";
-import Header from './components/Header';
+// import Header from './components/Header';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <Router>
-      <Header/>
-      {/* <Route path = '/test' exact component = {Header}/> */}
+      {/* <Header/> */}
+      <Route path = '/' exact component = {Welcome}/>
     </Router>
   );
 }
