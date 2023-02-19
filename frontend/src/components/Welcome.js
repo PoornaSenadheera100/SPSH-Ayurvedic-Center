1658093c8c802faffa367f976365ad3767fa4e24
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-02-19 08:21:19 +0530

    Implemented Welcome component

---
+++ frontend/src/components/Welcome.js
@@ -0,0 +1,8 @@
+export default function Welcome(){
+    return(
+        <div>
+            <h1>Welcome to SPSH Ayurvedic Center</h1>
+            <button>Buyer login</button>
+        </div>
+    )
+}
