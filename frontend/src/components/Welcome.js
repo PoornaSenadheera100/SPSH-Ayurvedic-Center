9f0ee029799b75bab3bdf4ba04da5e352b45d892
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-02-22 21:54:16 +0530

    Added signup btn

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -5,6 +5,7 @@ export default function Welcome(){
             <a href="/buyerlogin"><button>Buyer Login</button></a>
             <a href="/sellerlogin"><button>Seller Login</button></a>
             <a href="/adminlogin"><button>Admin Login</button></a>
+            <a href="/buyersignup"><button>Signup</button></a>
         </div>
     )
 }
