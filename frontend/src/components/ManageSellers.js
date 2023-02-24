5c2e2259d398b26aa6d4cc920b662c6beef230d1
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-02-24 23:25:35 +0530

    Displayed fetched sellers

--- frontend/src/components/ManageSellers.js
+++ frontend/src/components/ManageSellers.js
@@ -1,8 +1,66 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function ManageSellers(){
+
+    const [sellers, setSellers] = useState([]);
+    
+    useEffect(()=>{
+        function getSellers(){
+            axios.get("http://localhost:8070/seller/").then((res)=>{
+                setSellers(res.data);
+            }).catch((err)=>{
+                alert(err.message);
+            })
+        }
+        getSellers();
+    }, [])
+
     return(
         <div>
             <h1>Manage Sellers here</h1>
+
             <a href="/adminhome/managesellers/add"><button>Add</button></a>
+
+            {sellers.length === 0 && <h1>No Records</h1>}
+
+            {sellers.length !== 0 &&
+                <table className="table table-borderless" >
+                    <tr>
+                        <th><center>Name</center></th>
+                        <th><center>Email</center></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+
+                    <tbody>
+                        {
+                            sellers.map((seller)=>(
+                                <tr>
+                                    <td><center>{seller.name}</center></td>
+                                    <td><center>{seller.email}</center></td>
+                                    <td>
+                                        <button className="btn btn-success btn-sm" onClick={()=>{
+                                            window.location.replace(`http://localhost:3000/transport/update/${seller.email}`);
+                                        }}>View <i class="fa fa-pencil"></i></button>
+                                    </td>
+                                    <td>
+                                        <button className="btn btn-success btn-sm" onClick={()=>{
+                                            window.location.replace(`http://localhost:3000/transport/update/${seller.email}`);
+                                        }}>Update <i class="fa fa-pencil"></i></button>
+                                    </td>
+                                    <td>
+                                        <button className="btn btn-danger btn-sm" onClick={()=>{
+                                            window.location.replace(`http://localhost:3000/transport/delete/${seller.email}`);
+                                        }}>Delete <i class="fa fa-trash-o fa-lg"></i></button>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            }
         </div>
     )
 }
