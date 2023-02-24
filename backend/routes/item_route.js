74b49c3b0bab843c312982b790922e1e01b74ca3
HilarinaChristy <it21127946@my.sliit.lk>
2023-02-24 22:58:04 +0530

    Modified route file of item_route.js

--- backend/routes/item_route.js
+++ backend/routes/item_route.js
@@ -1,71 +1,58 @@
 const router = require("express").Router();
-let items = require("../models/Item");
+let Seller = require("../models/Item");
 
 router.route("/add").post((req, res)=>{
-    const product_id = req.body.id;
-    const product_name = req.body.name;
+    const productId = req.body.productId;
+    const name = req.body.name;
     const description = req.body.description;
+    const price = req.body.price;
     const quantity = req.body.quantity;
     const image = req.body.image;
 
 
     const newItem = new Item({
-        product_id,
-        product_name,
+        productId,
+        name,
         description,
         quantity,
-
+        image
     })
 
-    newCategory1.save().then(()=>{
-        res.json("Category1 Added.");
+    newItem.save().then(()=>{
+        res.json("Item Added.");
     }).catch((err)=>{
         console.log(err);
     })
 })
 
 router.route("/").get((req, res)=>{
-    Category1.find().then((category1)=>{
-        res.json(category1);
+    Item.find().then((item)=>{
+        res.json(item);
     }).catch((err)=>{
         console.log(err);
-    })
-})
-
-router.route("/update/:id").put(async(req, res)=>{
-    let category1Id = req.params.id;
-    const {name} = req.body;
-    const updateCategory1 = {
-        name
-    }
-
-    const update = await Category1.findByIdAndUpdate(category1Id, updateCategory1).then(()=>{
-        res.status(200).send({status: "Category1 Updated"})
-    }).catch((err)=>{
-        console.log(err);
-        res.status(500).send({status: "Error with updating Category1", error: err.message});
     })
 })
 
 router.route("/delete/:id").delete(async(req, res)=>{
-    let category1Id = req.params.id;
+    let itemId = req.params.id;
 
-    await Category1.findByIdAndDelete(category1Id).then(()=>{
-        res.status(200).send({status: "Category1 Deleted"});
+    await Item.findByIdAndDelete(itemId).then(()=>{
+        res.status(200).send({status: "Item Deleted"});
     }).catch((err)=>{
         console.log(err.message);
-        res.status(500).send({status: "Error with delete Category1", error: err.message});
+        res.status(500).send({status: "Error ", error: err.message});
     })
 })
 
 router.route("/get/:id").get(async(req, res)=>{
-    let category1Id = req.params.id;
-    const category1 = await Category1.findById(category1Id).then((category1)=>{
-        res.status(200).send({status: "Category1 fetched", category1})
+    let itemId = req.params.id;
+    const item = await Item.findById(itemId).then((item)=>{
+        res.status(200).send({status: "Item retrieved", item})
     }).catch((err)=>{
         console.log(err.message);
-        res.status(500).send({status: "Error with get category1", error: err.message});
+        res.status(500).send({status: "Error", error: err.message});
     })
 })
+
 
 module.exports = router;
