53b2556649ca609305cdf08a4780abb9776c2211
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-02-24 21:45:15 +0530

    Fetched all buyers

--- frontend/src/components/ManageBuyers.js
+++ frontend/src/components/ManageBuyers.js
@@ -1,4 +1,22 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function ManageBuyers(){
+
+    const [buyers, setBuyers] = useState({});
+    
+    useEffect(()=>{
+        function getBuyers(){
+            axios.get("http://localhost:8070/buyer/").then((res)=>{
+                console.log(res.data);
+                setBuyers(res.data);
+            }).catch((err)=>{
+                alert(err.message);
+            })
+        }
+        getBuyers();
+    }, [])
+
     return(
         <div>
             <h1>Manage Buyers here.</h1>
