637cbd6f56da9b1584e9e8d03035ea2a5fcb9c18
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-02-24 20:51:48 +0530

    Created manage sellers component

---
+++ frontend/src/components/ManageSellers.js
@@ -0,0 +1,8 @@
+export default function ManageSellers(){
+    return(
+        <div>
+            <h1>Manage Sellers here</h1>
+            <a href=""><button>Add</button></a>
+        </div>
+    )
+}
