645aded1ea499ae3952697b477e64780c30c79a2
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-02-24 20:56:39 +0530

    Added URL to seller add btn

--- frontend/src/components/ManageSellers.js
+++ frontend/src/components/ManageSellers.js
@@ -2,7 +2,7 @@ export default function ManageSellers(){
     return(
         <div>
             <h1>Manage Sellers here</h1>
-            <a href=""><button>Add</button></a>
+            <a href="/adminhome/managesellers/add"><button>Add</button></a>
         </div>
     )
 }
