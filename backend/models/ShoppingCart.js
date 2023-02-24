b5df81c6a8b49fa243762465c9c7cef5afe0c63a
SanuthiVihansa <subasofts@gmail.com>
2023-02-24 20:06:58 +0530

    Adding attributes to the Modules

--- backend/models/ShoppingCart.js
+++ backend/models/ShoppingCart.js
@@ -3,13 +3,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const cartShema = new Schema({
-    buyerID : {
+    buyerEmail: {
+        type: String,
+        required: true
 
     },
     itemID : {
-        type : Array,
+        type : String,
+        required: true
         
     },
+    ProductQty : {
+        type : String,
+        required: true
+
+    }
 
 })
 
