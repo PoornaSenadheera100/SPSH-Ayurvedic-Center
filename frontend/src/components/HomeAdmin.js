ba63772bc2a729aca57d024722da608b696f943c
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-02-24 20:40:41 +0530

    Added 2 buttons for admin home page

--- frontend/src/components/HomeAdmin.js
+++ frontend/src/components/HomeAdmin.js
@@ -2,6 +2,9 @@ export default function HomeAdmin(){
     return(
         <div>
             <h1>Welcome to Admin Home</h1>
+
+            <a href="/adminhome/managebuyers"><button>Manage Buyers</button></a>
+            <a href="/adminhome/managesellers"><button>Manage Sellers</button></a>
         </div>
     )
 }
