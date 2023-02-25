28ab3d3b9ebe9003e9dc09e1cbf562b9a2c91d84
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-02-25 08:35:50 +0530

    Implemented functioanlity of manage buyers

--- frontend/src/components/ManageBuyers.js
+++ frontend/src/components/ManageBuyers.js
@@ -3,12 +3,11 @@ import { useEffect, useState } from "react";
 
 export default function ManageBuyers(){
 
-    const [buyers, setBuyers] = useState({});
+    const [buyers, setBuyers] = useState([]);
     
     useEffect(()=>{
         function getBuyers(){
             axios.get("http://localhost:8070/buyer/").then((res)=>{
-                console.log(res.data);
                 setBuyers(res.data);
             }).catch((err)=>{
                 alert(err.message);
@@ -19,7 +18,49 @@ export default function ManageBuyers(){
 
     return(
         <div>
-            <h1>Manage Buyers here.</h1>
+            <h1>Manage Buyers here</h1>
+
+            <a href="/adminhome/managebuyers/add"><button>Add</button></a>
+
+            {buyers.length === 0 && <h1>No Records</h1>}
+
+            {buyers.length !== 0 &&
+                <table className="table table-borderless" >
+                    <tr>
+                        <th><center>Name</center></th>
+                        <th><center>Email</center></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+
+                    <tbody>
+                        {
+                            buyers.map((buyer)=>(
+                                <tr>
+                                    <td><center>{buyer.name}</center></td>
+                                    <td><center>{buyer.email}</center></td>
+                                    <td>
+                                        <button className="btn btn-success btn-sm" onClick={()=>{
+                                            window.location.replace(`http://localhost:3000/adminhome/managebuyers/view/${buyer.email}`);
+                                        }}>View <i class="fa fa-pencil"></i></button>
+                                    </td>
+                                    <td>
+                                        <button className="btn btn-success btn-sm" onClick={()=>{
+                                            window.location.replace(`http://localhost:3000/adminhome/managebuyers/update/${buyer.email}`);
+                                        }}>Update <i class="fa fa-pencil"></i></button>
+                                    </td>
+                                    <td>
+                                        <button className="btn btn-danger btn-sm" onClick={()=>{
+                                            window.location.replace(`http://localhost:3000/adminhome/managebuyers/delete/${buyer.email}`);
+                                        }}>Delete <i class="fa fa-trash-o fa-lg"></i></button>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            }
         </div>
     )
 }
