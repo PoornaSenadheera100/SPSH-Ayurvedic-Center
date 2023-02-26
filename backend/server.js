8bf3dad10e7cd2f2e2c2b099361914001c140e4b
Mona Lisa <it21127946@my.sliit.lk>
2023-02-26 23:27:55 +0530

    Allowing server to access routes file.

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,13 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 8070;
 
+//app uses the cors dependency package
 app.use(cors());
+
+//app uses the bodyParser package of the json format used by MongoDB
 app.use(bodyParser.json());
 
+//can assign the MONGODB_URL directly using the process.env
 const URL = process.env.MONGODB_URL;
 
 mongoose.set('strictQuery', true);
@@ -47,7 +51,15 @@ connection.once("open", ()=>{
 });
 
 
-/*type here*/
+//The server.js should be able to access the "inventories.js" file of the "routes folder."
+//The inventories.js file access path is assigned to a variable called "inventoryRouter"
+const itemRouter = require("./routes/item_route.js");
+
+//When passing data from frontend to backend, we use a URL and call the backend.(http://localhost:8070/inventory)  
+//There's a function called "use" in express.
+//Express was initially assigned to a variable called "app"
+//This function uses 2 parameters.  When first parameter is called --> it loads the inventories.js file in the routes folder.(Path is assigned to inventoryRouter variable)
+app.use("/item",itemRouter);
 
 app.post('/upload',(req,res)=>{
     upload(req,res,(err)=>{
