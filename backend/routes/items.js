99c114ad3c54626919bbc52810d724ccc2200c76
Mona Lisa <it21127946@my.sliit.lk>
2023-02-27 12:37:50 +0530

    Update Route of Items modified.

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -6,7 +6,7 @@ router.route("/add").post((req, res)=>{
     const name = req.body.name;
     const description = req.body.description;
     const price = req.body.price;
-    const quantity = req.body.quantity;
+    const quantity = Number(req.body.quantity);
     const image = req.body.image;
 
 
@@ -26,14 +26,16 @@ router.route("/add").post((req, res)=>{
     })
 })
 
+//RETRIEVE DETAILS ROUTE.
 router.route("/").get((req, res)=>{
-    Item.find().then((item)=>{
-        res.json(item);
+    Item.find().then((items)=>{
+        res.json(items);
     }).catch((err)=>{
         console.log(err);
     })
 })
 
+//DELETE ROUTE.
 router.route("/delete/:id").delete(async(req, res)=>{
     let itemId = req.params.id;
 
@@ -41,19 +43,46 @@ router.route("/delete/:id").delete(async(req, res)=>{
         res.status(200).send({status: "Item Deleted"});
     }).catch((err)=>{
         console.log(err.message);
-        res.status(500).send({status: "Error ", error: err.message});
+        res.status(500).send({status: "Error in deleting Item", error: err.message});
     })
 })
 
-router.route("/get/:id").get(async(req, res)=>{
+//RETRIEVEING ONE SPECIFIC DETAIL
+router.route("/get/:id").get(async(req,res) =>{
     let itemId = req.params.id;
-    const item = await Item.findById(itemId).then((item)=>{
-        res.status(200).send({status: "Item retrieved", item})
-    }).catch((err)=>{
+    const item = await Item.findById(itemId)
+    .then(()=>{
+        res.status(200).send({status:"Item fetched",item:item})
+    }).catch(()=>{
         console.log(err.message);
-        res.status(500).send({status: "Error", error: err.message});
+        res.status(500).send({status:"Error with getting one item",error:err.message});
     })
 })
 
+//UPDATE ROUTE
+router.route("/update/:id").put(async(req,res)=>{
+    let itemId = req.params.id;
+    const {productId,name,description,price,quantity,image} = req.body;
+
+    const updateItem = {
+        productId,
+        name,
+        description,
+        price,
+        quantity,
+        image
+    }
+
+    const update = await Item.findByIdAndUpdate(itemId,updateItem)
+    .then(()=>{
+        res.status(200).send({status : "Item Updated",item: update})
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).send({status:"Error with updating data", error:err.message});
+    })
+    
+   
+
+})
 
 module.exports = router;
