bd77b3f694e45fbc5cc842293b2f1c39d6b8322c
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-02-27 19:08:30 +0530

    Implemented the functionality of View Buyer

--- frontend/src/components/ViewBuyer.js
+++ frontend/src/components/ViewBuyer.js
@@ -1,7 +1,42 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+
 export default function ViewBuyer(){
+    
+    const {email} = useParams();
+    const [name, setName] = useState("");
+    const [address, setAddress] = useState("");
+    const [nic, setNic] = useState("");
+    const [phone, setPhone] = useState("");
+
+    useEffect(()=>{
+        axios.get(`http://localhost:8070/buyer/get/email/${email}`).then((res)=>{
+            console.log(res.data);
+            setName(res.data[0].name);
+            setAddress(res.data[0].address);
+            setNic(res.data[0].nic);
+            setPhone(res.data[0].phone);
+        }).catch((err)=>{
+            alert('Network Issue...');
+        })
+    });
+
     return(
         <div>
+            <a href="/adminhome/managebuyers"><button>Back</button></a>
             <h1>View Buyer</h1>
+
+            Name &nbsp;&nbsp;&nbsp; : {name}
+            <br></br>
+            address &nbsp; : {address}
+            <br></br>
+            nic &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : {nic}
+            <br></br>
+            email &nbsp;&nbsp;&nbsp;&nbsp; : {email}
+            <br></br>
+            phone &nbsp;&nbsp;&nbsp; : {phone}
+            <br></br>
         </div>
     )
 }
