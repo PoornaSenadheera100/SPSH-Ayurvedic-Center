5cd208bd554bd8cba0d3e3862462bb0aa5a2f055
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-02-28 21:30:00 +0530

    Added signout option

--- frontend/src/components/HomeAdmin.js
+++ frontend/src/components/HomeAdmin.js
@@ -1,6 +1,8 @@
 export default function HomeAdmin(){
     return(
         <div>
+            <a href="/"><button>Signout</button></a>
+
             <h1>Welcome to Admin Home</h1>
 
             <a href="/adminhome/managebuyers"><button>Manage Buyers</button></a>
