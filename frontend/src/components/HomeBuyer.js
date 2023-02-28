3c4431a8e5db8644ae63483bcbda580cb5a3193c
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-02-28 22:44:37 +0530

    Added signout options for buyers and sellers

--- frontend/src/components/HomeBuyer.js
+++ frontend/src/components/HomeBuyer.js
@@ -1,6 +1,8 @@
 export default function HomeBuyer(){
     return(
         <div>
+            <a href="/"><button>Signout</button></a>
+            
             <h1>Welcome to Buyer Home</h1>
         </div>
     )
--- frontend/src/components/HomeSeller.js
+++ frontend/src/components/HomeSeller.js
@@ -1,6 +1,8 @@
 export default function HomeSeller(){
     return(
         <div>
+            <a href="/"><button>Signout</button></a>
+            
             <h1>This is Seller Home</h1>
         </div>
     )
