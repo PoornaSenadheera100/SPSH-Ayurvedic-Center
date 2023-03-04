068ff287ba528103f18b8ae9a9b177635724bb59
SanuthiVihansa <subasofts@gmail.com>
2023-03-04 10:21:29 +0530

    commit changes in the buyerhomepage

--- frontend/src/components/HomeBuyer.js
+++ frontend/src/components/HomeBuyer.js
@@ -1,8 +1,27 @@
-export default function HomeBuyer(){
+import React ,{useState,useEffect} from "react";
+import axios from "axios";
+import { useHistory} from "react-router-dom";
 
+export default function HomeBuyer(){
+//session validation
     if(sessionStorage.getItem("sAyurCenReyub") === null){
         window.location.replace("/buyerlogin");
     }
+
+    useEffect(()=>{
+        function getItems(){
+            axios.get("http://localhost:8070/leave/").then((res)=>{
+                console.log(res.data);
+                setLeaves(res.data);
+            }).catch((err)=>{
+                alert(err.message);
+            })
+        }
+        getLeaves();
+
+    },[])
+
+
 
     return(
         <div>
