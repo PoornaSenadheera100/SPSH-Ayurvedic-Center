83312eff3f51d63845b198985a3f8e69af359c90
SanuthiVihansa <subasofts@gmail.com>
2023-03-04 10:09:39 +0530

    commit welcome.js changes

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -1,7 +1,7 @@
 export default function Welcome(){
     return(
         <div>
-            <h1>Welcome to SPSH Ayurvedic Center</h1>
+            <center><h1>Welcome to SPSH Ayurvedic Center</h1></center>
             <a href="/buyerlogin"><button>Buyer Login</button></a>
             <a href="/sellerlogin"><button>Seller Login</button></a>
             <a href="/adminlogin"><button>Admin Login</button></a>
