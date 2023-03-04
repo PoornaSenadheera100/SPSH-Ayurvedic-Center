296aa09f07bbdf5d64ca9a7c3ad4fa1bda48c699
SanuthiVihansa <subasofts@gmail.com>
2023-03-04 10:21:57 +0530

    commit welcome.js

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -5,11 +5,7 @@ export default function Welcome(){
             <a href="/buyerlogin"><button>Buyer Login</button></a>
             <a href="/sellerlogin"><button>Seller Login</button></a>
             <a href="/adminlogin"><button>Admin Login</button></a>
-            <a href="/buyersignup"><button>Signup</button></a>
-
-            <br></br>
-            <center><h1>Featured products</h1></center>
-            
+            <a href="/buyersignup"><button>Signup</button></a>            
         </div>
     )
 }
