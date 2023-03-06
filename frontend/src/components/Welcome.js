62dca60b509af4b82d01eddba848c16aa25d8cb9
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-03-06 10:22:12 +0530

    Added container

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -1,11 +1,13 @@
 export default function Welcome(){
     return(
-        <div>
-            <h1>Welcome to SPSH Ayurvedic Center</h1>
-            <a href="/buyerlogin"><button>Buyer Login</button></a>
-            <a href="/sellerlogin"><button>Seller Login</button></a>
-            <a href="/adminlogin"><button>Admin Login</button></a>
-            <a href="/buyersignup"><button>Signup</button></a>
+        <div className="container">
+            <center>
+                <h1>Welcome to SPSH Ayurvedic Center</h1>
+                <a href="/buyerlogin"><button>Buyer Login</button></a>
+                <a href="/sellerlogin"><button>Seller Login</button></a>
+                <a href="/adminlogin"><button>Admin Login</button></a>
+                <a href="/buyersignup"><button>Signup</button></a>
+            </center>
         </div>
     )
 }
