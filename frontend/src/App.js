d75e7fb7fb565c26292d7b14589677a53cf8344b
Mona Lisa <it21127946@my.sliit.lk>
2023-03-06 07:19:19 +0530

    Added route for "All Items"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddItem from './components/AddItem';
+import AllItems from './components/AllItems';
 
 function App() {
   return (
     <Router>
     <div className="App">
       <Route path="/item/add/" exact component={AddItem}></Route>
-     
+     <Route path="/item" exact component={AllItems}></Route>
     </div>
     </Router>
   );
