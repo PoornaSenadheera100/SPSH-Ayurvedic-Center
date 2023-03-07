e256f02e6d3a3a44b88bb075e9487d0d6b170f43
Mona Lisa <it21127946@my.sliit.lk>
2023-03-07 15:51:22 +0530

    Delete Component of Items.

--- frontend/src/components/DeleteItem.js
+++ frontend/src/components/DeleteItem.js
@@ -45,10 +45,10 @@ export default function DeleteItem() {
             <form onSubmit={deleteData}>
                 <div className="form-group">
                     <div style={{ marginLeft: "0px", marginRight: "auto", width: "10%" }}>
-                        <label for="name">Product ID</label>
+                        <label for="product_id">Product ID</label>
                     </div>
                     <div class="col-sm-10">
-                        <input type="text" className="form-control" id="name" value={ProductId} placeholder="Enter Item Code" disabled />
+                        <input type="text" className="form-control" id="product_id" value={ProductId} placeholder="Enter Item Code" disabled />
                     </div>
                 </div>
                 <div className="form-group">
@@ -57,43 +57,43 @@ export default function DeleteItem() {
                     </div>
                     <div class="col-sm-10">
                         {/* using the value --> we can display the values that was previously entered by the user.*/}
-                        <input type="text" className="form-control" id="age" value={Name} placeholder="Enter Description" disabled />
+                        <input type="text" className="form-control" id="age" value={Name} placeholder="Enter Name" disabled />
                     </div>
                 </div>
                 <div className="form-group">
                     <div style={{ marginLeft: "0px", marginRight: "auto", width: "10%" }}>
-                        <label for="gender">Description</label>
+                        <label for="description">Description</label>
                     </div>
                     <div class="col-sm-10">
-                        <input type="text" className="form-control" id="gender" value={Description} placeholder="Enter Invoice No" disabled />
-                    </div>
-                </div>
-
-                <div className="form-group">
-                    <div style={{ marginLeft: "0px", marginRight: "auto", width: "10%" }}>
-                        <label for="gender">Price</label>
-                    </div>
-                    <div class="col-sm-10">
-                        <input type="number" className="form-control" id="quantity" value={Price} placeholder="Enter quantity" disabled />
+                        <input type="text" className="form-control" id="description" value={Description} placeholder="Enter Description" disabled />
                     </div>
                 </div>
 
                 <div className="form-group">
                     <div style={{ marginLeft: "0px", marginRight: "auto", width: "10%" }}>
-                        <label for="gender">Quantity</label>
+                        <label for="price">Price</label>
                     </div>
                     <div class="col-sm-10">
-                        <input type="text" className="form-control" id="supplier" value={Quantity} placeholder="Enter supplier" disabled />
+                        <input type="number" className="form-control" id="price" value={Price} placeholder="Enter Price" disabled />
                     </div>
                 </div>
 
                 <div className="form-group">
                     <div style={{ marginLeft: "0px", marginRight: "auto", width: "10%" }}>
-                        <label for="gender">Image</label>
+                        <label for="quantity">Quantity</label>
+                    </div>
+                    <div class="col-sm-10">
+                        <input type="text" className="form-control" id="quantity" value={Quantity} placeholder="Enter Quantity" disabled />
+                    </div>
+                </div>
+
+                <div className="form-group">
+                    <div style={{ marginLeft: "0px", marginRight: "auto", width: "10%" }}>
+                        <label for="image">Image</label>
                     </div>
 
                     <div class="col-sm-10">
-                        <input type="text" className="form-control" id="date" value={Image} placeholder="Enter Order Date" disabled />
+                        <input type="file" className="form-control" id="image" value={Image} placeholder="Upload Image" disabled />
 
                     </div>
                 </div>
