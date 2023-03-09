d625d4234e602d060d47c36fd1c4b6d6d4a7319e
Mona Lisa <it21127946@my.sliit.lk>
2023-03-09 10:30:38 +0530

    Update Route added.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddItem from './components/AddItem';
 import DeleteItem from './components/DeleteItem';
 import AllItems from './components/AllItems';
+import UpdateItem from './components/UpdateItem';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/item/add/" exact component={AddItem}></Route>
      <Route path="/item" exact component={AllItems}></Route>
      <Route path="/item/delete/:id" exact componenet={DeleteItem}></Route>
+     <Route path="/item/update/:id" exact component={UpdateItem}></Route>
     </div>
     </Router>
   );
