ef3b12eccf07cad3570e302843909b795bcdd0c4
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-03-13 19:07:48 +0530

    Commented email sender

--- backend/server.js
+++ backend/server.js
@@ -101,20 +101,20 @@ app.use("/item",itemRouter);
 
 
 
-const sgMail = require('@sendgrid/mail')
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-const msg = {
-    to: 'poornasenadheeraonline@gmail.com', // Change to your recipient
-    from: 'admin@spsh.lk', // Change to your verified sender
-    subject: 'Registration Successfull',
-    text: 'This is Text',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-}
-sgMail.send(msg).then(() => {
-    console.log('Email sent')
-}).catch((error) => {
-    console.error(error)
-})
+// const sgMail = require('@sendgrid/mail')
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+// const msg = {
+//     to: 'poornasenadheeraonline@gmail.com', // Change to your recipient
+//     from: 'admin@spsh.lk', // Change to your verified sender
+//     subject: 'Registration Successfull',
+//     text: 'This is Text',
+//     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+// }
+// sgMail.send(msg).then(() => {
+//     console.log('Email sent')
+// }).catch((error) => {
+//     console.error(error)
+// })
 
 
 
