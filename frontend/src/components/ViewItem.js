a892b491269d6affac87a8b3a1b9a0a4dde2be50
Sathira-Wijeratne <swije27@gmail.com>
2023-03-20 22:54:48 +0530

    implemention done halfway

--- frontend/src/components/ViewItem.js
+++ frontend/src/components/ViewItem.js
@@ -8,30 +8,30 @@ export default function ViewItem(){
     //     window.location.replace("/adminlogin");
     // }
     
-    // const {email} = useParams();
-    // const [name, setName] = useState("");
-    // const [phone, setPhone] = useState("");
+    const {email} = useParams();
+    const [name, setName] = useState("");
+    const [phone, setPhone] = useState("");
 
-    // useEffect(()=>{
-    //     axios.get(`http://localhost:8070/seller/get/email/${email}`).then((res)=>{
-    //         setName(res.data[0].name);
-    //         setPhone(res.data[0].phone);
-    //     }).catch((err)=>{
-    //         alert('Network Issue...');
-    //     })
-    // });
+    useEffect(()=>{
+        axios.get(`http://localhost:8070/seller/get/email/${email}`).then((res)=>{
+            setName(res.data[0].name);
+            setPhone(res.data[0].phone);
+        }).catch((err)=>{
+            alert('Network Issue...');
+        })
+    });
 
-    // return(
-    //     <div>
-    //         <a href="/adminhome/managesellers"><button>Back</button></a>
-    //         <h1>View Seller</h1>
+    return(
+        <div>
+            <a href="/adminhome/managesellers"><button>Back</button></a>
+            <h1>View Seller</h1>
 
-    //         Name &nbsp;&nbsp;&nbsp; : {name}
-    //         <br></br>
-    //         email &nbsp;&nbsp;&nbsp;&nbsp; : {email}
-    //         <br></br>
-    //         phone &nbsp;&nbsp;&nbsp; : {phone}
-    //         <br></br>
-    //     </div>
-    // )
+            Name &nbsp;&nbsp;&nbsp; : {name}
+            <br></br>
+            email &nbsp;&nbsp;&nbsp;&nbsp; : {email}
+            <br></br>
+            phone &nbsp;&nbsp;&nbsp; : {phone}
+            <br></br>
+        </div>
+    )
 }
