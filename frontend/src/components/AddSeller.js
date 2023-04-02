8b3f3f3998eefee59093eb8fdce89dfbad143a25
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-04-02 18:54:13 +0530

    Added container

--- frontend/src/components/AddSeller.js
+++ frontend/src/components/AddSeller.js
@@ -47,7 +47,7 @@ export default function AddSeller(){
     }
 
     return(
-        <div>
+        <div className="container">
             <a href="/adminhome/managesellers"><button>Back</button></a>
             
             <h1>Create a Seller Account</h1>
