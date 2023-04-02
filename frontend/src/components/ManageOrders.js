200bcfd9f993fbe2295ee5cec7f47866a7456eca
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-04-02 17:45:20 +0530

    Added authentication method to ManageOrders page

--- frontend/src/components/ManageOrders.js
+++ frontend/src/components/ManageOrders.js
@@ -1,4 +1,9 @@
 export default function ManageOrders(){
+
+    if(sessionStorage.getItem("sAyurCenNimda") === null){
+        window.location.replace("/adminlogin");
+    }
+
     return(
         <div className = "container">
             <h1>Manage Orders</h1>
