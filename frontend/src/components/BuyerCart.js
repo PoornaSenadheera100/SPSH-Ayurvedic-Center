f3f32f706550e552b8cd42099eed6122d6f26019
Sathira-Wijeratne <swije27@gmail.com>
2023-04-03 23:12:13 +0530

    created BuyerCart file

---
+++ frontend/src/components/BuyerCart.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+
+import { Buffer } from 'buffer';
+
+export default function BuyerCart(){
+    //session validation
+    if (sessionStorage.getItem("sAyurCenReyub") === null) {
+        window.location.replace("/buyerlogin");
+    }
+
+
+    //creating variables
+    const [items, setItems] = useState([]);
+
+    let history = useHistory();
+
+    const buyerEmail = sessionStorage.getItem("buyerEmail");
+
+    useEffect(() => {
+        function getItems(){
+            axios.get()
+        }
+    })
+
+    return{
+
+    }
+}
+
