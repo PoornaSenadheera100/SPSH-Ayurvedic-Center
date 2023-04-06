c889f558707e1cf65e8081b1c25294050b6b0359
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-04-06 23:59:23 +0530

    Designed the page checkout

--- frontend/src/components/Checkout.js
+++ frontend/src/components/Checkout.js
@@ -1,17 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function Checkout(){
+
+    const email = sessionStorage.getItem("buyerEmail");
+    const [name, setName] = useState("");
+    const [address, setAddress] = useState("");
+    const [nic, setNic] = useState("");
+    const [phone, setPhone] = useState("");
+
+    useEffect(()=>{
+        axios.get(`http://localhost:8070/buyer/get/email/${email}`).then((res)=>{
+            setName(res.data[0].name);
+            setAddress(res.data[0].address);
+            setNic(res.data[0].nic);
+            setPhone(res.data[0].phone);
+        }).catch((err)=>{
+            alert('Network Issue...');
+        })
+    });
+
     return(
         <div className="container">
             <a type="button" href="/buyer/view/cart" class="btn btn-secondary">Back</a>
             <h1>Checkout page</h1>
-            Your Details
-            Name
-            Address
-            NIC
-            Email
-            Phone
-
-            Total Amount
-            Delivery Charge
+            <b>Your Details</b><br/>
+            Name : {name} <br/>
+            Address : {address} <br/>
+            NIC : {nic} <br/>
+            Email : {email} <br/>
+            Phone : {phone} <br/><br/>
+            
+            <b>Total Amount</b> : <br/>
+            <b>Delivery Charge</b> : <br/><br/>
 
             Select the delivery agent
 
@@ -19,7 +40,7 @@ export default function Checkout(){
             Credit / Debit Card (Online)
             Cash on Delivery
 
-            
+
 
         </div>
     )
