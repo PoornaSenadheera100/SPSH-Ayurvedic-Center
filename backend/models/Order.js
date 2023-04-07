a235441f8060d58768dc8660023295f7c1ff3a39
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-04-07 16:54:06 +0530

    Added Order model

---
+++ backend/models/Order.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+    buyeremail : {
+        type : String,
+        required : true
+    },
+
+    buyername : {
+        type : String,
+        required : true
+    },
+
+    buyerphone : {
+        type : String,
+        required : true
+    },
+
+    buyeraddress : {
+        type : String,
+        required : true
+    },
+
+    buyernic : {
+        type : String,
+        required : true
+    },
+
+    totalamount : {
+        type : String,
+        required : true
+    },
+
+    deliveryagent : {
+        type : String,
+        required : true
+    },
+
+    paymentmethod : {
+        type : String,
+        required : true
+    },
+
+    status : {
+        type : String,
+        required : true
+    }
+})
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = Order;
