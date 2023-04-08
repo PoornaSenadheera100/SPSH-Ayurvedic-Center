43628280bcb226d293680e5720016d5860196e64
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-04-08 07:53:21 +0530

    Created MyOrders component

---
+++ frontend/src/components/MyOrders.js
@@ -0,0 +1,12 @@
+export default function MyOrders(){
+
+    
+
+
+    return(
+        <div className="container">
+            <a href="/buyerhome"><button className="btn btn-dark">Back</button></a>
+            <center><h1>My Orders</h1></center>
+        </div>
+    )
+}
