899e2cd59429a15f24670279e636ae0edec9d8d4
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-04-08 12:44:14 +0530

    Implemented MyOrder.js

--- frontend/src/components/MyOrders.js
+++ frontend/src/components/MyOrders.js
@@ -11,13 +11,45 @@ export default function MyOrders(){
             setOrders(res.data);
             console.log(res.data);
         })
-    })
+    }, [])
 
 
     return(
         <div className="container">
             <a href="/buyerhome"><button className="btn btn-dark">Back</button></a>
             <center><h1>My Orders</h1></center>
+
+            {orders.length === 0 && <h1>No Orders</h1>}
+
+            {orders.length !== 0 &&
+                <table className="table table-borderless" >
+                    <tr>
+                        <th><center>Reference No.</center></th>
+                        <th><center>Payment Method</center></th>
+                        <th><center>Payement Status</center></th>
+                        <th><center>Approval Status</center></th>
+                        <th></th>
+                    </tr>
+
+                    <tbody>
+                        {
+                            orders.map((order)=>(
+                                <tr>
+                                    <td><center>{order.orderRef}</center></td>
+                                    <td><center>{order.paymentmethod}</center></td>
+                                    <td><center>{order.status}</center></td>
+                                    <td><center>{order.appStatus}</center></td>
+                                    <td>
+                                        <button className="btn btn-warning btn-sm" onClick={()=>{
+                                            window.location.replace(`http://localhost:3000/buyerhome/myorders/${order.orderRef}`);
+                                        }}>View <i class="fa fa-pencil"></i></button>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            }
         </div>
     )
 }
