08940bc53420e49d89db7d98bf1e921aa961053c
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-04-09 19:55:51 +0530

    Applied new CSS

--- frontend/src/components/BuyerViewItem.js
+++ frontend/src/components/BuyerViewItem.js
@@ -98,74 +98,109 @@ export default function BuyerViewItem() {
 
     if (Image !== "") {
         return (
-            <div className="container">
-                <div><a type="button" href="/buyerhome" class="btn btn-secondary">Back</a></div>
-                <form onSubmit={add}>
-                {/* <table>
-                    <th>
-                        <h1>{Name}</h1>
-                        <h3>{ProductId} " : "{Description}</h3>
-                        <h3>{SupplierId}</h3>
-                        <h3>{Price}</h3>
-                        <h3>{Quantity}</h3>
-                        <input type={'number'} min = "1" value={Quantity} onChange={(e) => {setQuantity(e.target.value);}} ></input>
-                    </th>
-                        <th><img src={getImageSource(Image)} alt={Name} width="300px" /><bt></bt>
-                        <button className="btn btn-success" style={{ marginTop: '10px' }} onClick={(e) => {add(e);
-                                // window.location.replace(`http://localhost:3000/buyer/view/item/${id}`);
-                            }}>Add to cart</button>
-                        </th>
-                    </table> */}
-                    {/* <table className="table table-borderless">
-                        <tr>
-                            <th scope="col">Product ID</th>
-                            <th scope="col">Supplier ID</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Description</th>
-                            <th scope="col">Price</th>
-                            <th scope="col">Quantity</th>
-                            <th scope="col">Image</th>
-                        </tr>
-                        <tr scope="row">
-                            <td class="text-uppercase">{ProductId}</td>
-                            <td class="text-uppercase">{SupplierId}</td>
-                            <td class="text-uppercase">{Name}</td>
-                            <td class="text-uppercase">{Description}</td>
-                            <td class="text-uppercase">{Price}</td>
-                            {/* <td class="text-uppercase">{Quantity}</td> */}
-                        <table className="table table-borderless">
-                        <tr>
-                            <th scope="col">Product ID</th>
-                            <th scope="col">Supplier ID</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Description</th>
-                            <th scope="col">Price</th>
-                            <th scope="col">Quantity</th>
-                            <th scope="col">Image</th>
-                        </tr>
-                        <tr scope="row">
-                            <td class="text-uppercase">{ProductId}</td>
-                            <td class="text-uppercase">{SupplierId}</td>
-                            <td class="text-uppercase">{Name}</td>
-                            <td class="text-uppercase">{Description}</td>
-                            <td class="text-uppercase">{Price}</td>
-                            {/* <td class="text-uppercase">{Quantity}</td> */}
-                            <td><input type={'number'} min = "1" max={MaxQuantity} value={Quantity} onChange={(e) => {
-                                setQuantity(e.target.value);
-                            }} ></input></td>
-                            <td><img src={getImageSource(Image)} alt={Name} width="300px" /></td>
-                            <td><button className="btn btn-success" style={{ marginTop: '10px' }} onClick={(e) => {
-                                add(e);
-                                // window.location.replace(`http://localhost:3000/buyer/view/item/${id}`);
-                            }}>Add to cart</button></td>
-                        </tr>
-                    </table>
 
-                    <b>Rate the Product</b> <br/>
-                    <Rater onRate={(value)=>{
-                        rateProduct(value.rating);
-                    }} total={5} rating={rate} style={{ fontSize: '40px' }}/>
-                </form>
+            // Sathira's implementation - Begin
+            // <div className="container">
+            //     <div><a type="button" href="/buyerhome" class="btn btn-secondary">Back</a></div>
+            //     <form onSubmit={add}>
+            //     {/* <table>
+            //         <th>
+            //             <h1>{Name}</h1>
+            //             <h3>{ProductId} " : "{Description}</h3>
+            //             <h3>{SupplierId}</h3>
+            //             <h3>{Price}</h3>
+            //             <h3>{Quantity}</h3>
+            //             <input type={'number'} min = "1" value={Quantity} onChange={(e) => {setQuantity(e.target.value);}} ></input>
+            //         </th>
+            //             <th><img src={getImageSource(Image)} alt={Name} width="300px" /><bt></bt>
+            //             <button className="btn btn-success" style={{ marginTop: '10px' }} onClick={(e) => {add(e);
+            //                     // window.location.replace(`http://localhost:3000/buyer/view/item/${id}`);
+            //                 }}>Add to cart</button>
+            //             </th>
+            //         </table> */}
+            //         {/* <table className="table table-borderless">
+            //             <tr>
+            //                 <th scope="col">Product ID</th>
+            //                 <th scope="col">Supplier ID</th>
+            //                 <th scope="col">Name</th>
+            //                 <th scope="col">Description</th>
+            //                 <th scope="col">Price</th>
+            //                 <th scope="col">Quantity</th>
+            //                 <th scope="col">Image</th>
+            //             </tr>
+            //             <tr scope="row">
+            //                 <td class="text-uppercase">{ProductId}</td>
+            //                 <td class="text-uppercase">{SupplierId}</td>
+            //                 <td class="text-uppercase">{Name}</td>
+            //                 <td class="text-uppercase">{Description}</td>
+            //                 <td class="text-uppercase">{Price}</td>
+            //                 {/* <td class="text-uppercase">{Quantity}</td> */}
+            //             <table className="table table-borderless">
+            //             <tr>
+            //                 <th scope="col">Product ID</th>
+            //                 <th scope="col">Supplier ID</th>
+            //                 <th scope="col">Name</th>
+            //                 <th scope="col">Description</th>
+            //                 <th scope="col">Price</th>
+            //                 <th scope="col">Quantity</th>
+            //                 <th scope="col">Image</th>
+            //             </tr>
+            //             <tr scope="row">
+            //                 <td class="text-uppercase">{ProductId}</td>
+            //                 <td class="text-uppercase">{SupplierId}</td>
+            //                 <td class="text-uppercase">{Name}</td>
+            //                 <td class="text-uppercase">{Description}</td>
+            //                 <td class="text-uppercase">{Price}</td>
+            //                 {/* <td class="text-uppercase">{Quantity}</td> */}
+            //                 <td><input type={'number'} min = "1" max={MaxQuantity} value={Quantity} onChange={(e) => {
+            //                     setQuantity(e.target.value);
+            //                 }} ></input></td>
+            //                 <td><img src={getImageSource(Image)} alt={Name} width="300px" /></td>
+            //                 <td><button className="btn btn-success" style={{ marginTop: '10px' }} onClick={(e) => {
+            //                     add(e);
+            //                     // window.location.replace(`http://localhost:3000/buyer/view/item/${id}`);
+            //                 }}>Add to cart</button></td>
+            //             </tr>
+            //         </table>
+
+            //         <b>Rate the Product</b> <br/>
+            //         <Rater onRate={(value)=>{
+            //             rateProduct(value.rating);
+            //         }} total={5} rating={rate} style={{ fontSize: '40px' }}/>
+            //     </form>
+            // </div>
+            // Sathira's implementation - End
+
+            <div className="container">
+                 <div style={{padding: '10px'}}><a type="button" href="/buyerhome" className="btn btn-secondary">Back</a></div>
+                    <div align="center" style={{border: '1px solid black', borderRadius: '5px', padding: '10px', maxWidth: '500px', margin: '0 auto'}}>
+                        <div align="center">
+                            <form onSubmit={add}>
+                                <div style={{border: '1px solid #ccc', borderRadius: '5px', padding: '10px'}}>
+                                    <div style={{textAlign: 'center', marginBottom: '10px'}}>
+                                        <h1>{Name}</h1>
+                                    </div>
+                                 <div style={{textAlign: 'center'}}>
+                                     <img src={getImageSource(Image)} alt={Name} style={{maxWidth: '100%', height: 'auto'}} />
+                                </div>
+                                    <div style={{marginTop: '10px', textAlign:"left"}}>
+                                        {Description}<br />
+                                        PID &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: {ProductId}<br />
+                                        Quantity :
+                                        <input for = "qunatitytxt" type="number" min="1" defaultValue="1" max={MaxQuantity}  onChange={(e) => { setQuantity(e.target.value);}} style={{marginLeft: '5px'}} /><br />
+                                        Price &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:  {Price}<br />
+                                      
+                                        Supplier : {SupplierId} <br/>
+                                        <b>Rate the Product</b> &nbsp;
+                                        <Rater onRate={(value)=>{
+                                            rateProduct(value.rating);
+                                        }} total={5} rating={rate} style={{ fontSize: '30px' }}/>
+                                    </div>
+                                </div>
+                                <button className="btn btn-success" style={{marginTop: '10px', width: '100%'}} onClick={(e) => { add(e); }}>Add to cart</button>
+                            </form>
+                        </div>
+                    </div>
             </div>
         )
     }
