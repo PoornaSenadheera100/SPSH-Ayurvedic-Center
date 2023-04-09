8822b9793f9d841a4932f90fb5832863569f98a3
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-04-09 17:54:03 +0530

    Added required routes to rate.js

--- backend/routes/rate.js
+++ backend/routes/rate.js
@@ -4,15 +4,13 @@ let Rate = require("../models/Rate");
 router.route("/add").post((req, res)=>{
     const itemID = req.body.itemID;
     const buyerEmail = req.body.buyerEmail;
-    const ratingCount = Number(req.body.ratingCount);
-    const avgRate = Number(req.body.avgRate);
+    const rate = Number(req.body.rate);
 
 
     const newRate = new Rate({
         itemID,
         buyerEmail,
-        ratingCount,
-        avgRate
+        rate
     })
 
     newRate.save().then(()=>{
@@ -27,6 +25,45 @@ router.route("/").get((req, res)=>{
         res.json(rate);
     }).catch((err)=>{
         console.log(err);
+    })
+})
+
+router.route("/get/:itemID").get(async(req,res)=>{
+    let itemID = req.params.itemID;
+    
+    await Rate.find({"itemID": itemID}).then((rate)=>{
+        res.json(rate);
+    }).catch((err)=>{
+        console.log(err);
+        res.status(500).send({status:"Opps! Error in loading the rates"});
+    })
+})
+
+router.route("/get/:buyerEmail/:itemID").get(async(req,res)=>{
+    let buyerEmail = req.params.buyerEmail;
+    let itemID = req.params.itemID;
+    
+    await Rate.find({"buyerEmail": buyerEmail, "itemID": itemID}).then((rate)=>{
+        res.json(rate);
+    }).catch((err)=>{
+        console.log(err);
+        res.status(500).send({status:"Opps! Error in loading the rates"});
+    })
+})
+
+router.route("/update").put(async(req, res)=>{
+    const {itemID, buyerEmail, rate} = req.body;
+    const updateRate = {
+        itemID,
+        buyerEmail,
+        rate
+    }
+
+    await Rate.findOneAndUpdate({"buyerEmail" : buyerEmail, "itemID": itemID}, updateRate).then(()=>{
+        res.status(200).send({status: "Rate Updated"});
+    }).catch((err)=>{
+        console.log(err);
+        res.status(500).send({status: "Error with updating the rate", error: err.message});
     })
 })
 
