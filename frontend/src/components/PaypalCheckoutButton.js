6e03efd070062c67fe26c32288b6ea1269720faa
Sathira-Wijeratne <swije27@gmail.com>
2023-04-10 22:49:23 +0530

    implement PayPal Button and styles

--- frontend/src/components/PaypalCheckoutButton.js
+++ frontend/src/components/PaypalCheckoutButton.js
@@ -1,10 +1,18 @@
+import {PayPalButtons} from "@paypal/react-paypal-js"
+
 //holds logic and styling for checkout button
 const PaypalCheckoutButton = (props) => {//contains product
     const {product} = props;
 
-    return(
-        <></>
-    )
+    return <PayPalButtons
+    style={{
+        color: "silver",
+        layout: "horizontal",
+        height: 48,
+        tagline: false,
+        shape: "pill"
+    }}
+    />;
 };
 
 export default PaypalCheckoutButton;
