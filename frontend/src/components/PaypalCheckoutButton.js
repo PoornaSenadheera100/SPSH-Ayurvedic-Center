d1e64201dd0cddc59fac44f58b55c296ca447ab9
Sathira-Wijeratne <swije27@gmail.com>
2023-04-10 20:41:12 +0530

    PaypalCheckoutButton compoennt basic structure

---
+++ frontend/src/components/PaypalCheckoutButton.js
@@ -0,0 +1,10 @@
+//holds logic and styling for checkout button
+const PaypalCheckoutButton = (props) => {//contains product
+    const {product} = props;
+
+    return(
+        <></>
+    )
+};
+
+export default PaypalCheckoutButton;
