45e1e10157585d1e4aa36ed4ad1aa39a7e34bfe7
SanuthiVihansa <subasofts@gmail.com>
2023-04-15 13:37:18 +0530

    Removed unnessary comments

--- frontend/src/components/LoginBuyer.js
+++ frontend/src/components/LoginBuyer.js
@@ -29,8 +29,7 @@ export default function LoginBuyer(){
 
     return(
         <div className="container">
-            {/* <a href="/"><button className="btn btn-primary">Back</button></a> */}
-<a href="/"><Button variant="dark">Back</Button></a>
+            <a href="/"><Button variant="dark">Back</Button></a>
             <form onSubmit={validate}>
                 <section className="vh-100 gradient-custom">
                     <div className="container py-5 h-100">
