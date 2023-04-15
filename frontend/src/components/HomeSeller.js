eaee111ada9be3f1535b37f2c34e590115b6907f
SanuthiVihansa <subasofts@gmail.com>
2023-04-16 02:16:08 +0530

    add changes

--- frontend/src/components/HomeSeller.js
+++ frontend/src/components/HomeSeller.js
@@ -13,12 +13,22 @@ export default function HomeSeller(){
             
             <center><h1>Seller Home</h1></center>
             {/* <h1>Hello {sessionStorage.getItem("sellerEmail")}</h1> */}
+            <div align="center" style={{ border: '1px solid black', borderRadius: '5px', padding: '20px', maxWidth: '500px', margin: '0 auto', background: '#f8f9fa' }}>
+    <center style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <a href="/sellerhome/item">
+            <button className="btn btn-primary btn-lg" style={{ width: '200px', height: '200px' }}>
+                Manage Items
+            </button>
+        </a>
+        {'      '}
+        <a href="/sellerhome/delivery">
+            <button className="btn btn-primary btn-lg" style={{ width: '200px', height: '200px' }}>
+                Delivery Requests
+            </button>
+        </a>
+    </center>
+</div>
 
-            <center>
-                <a href="/sellerhome/item"><button className="btn btn-primary btn-lg">Manage Items</button></a>
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                <a href="/sellerhome/delivery"><button className="btn btn-primary btn-lg">Delivery Requests</button></a>
-            </center>
-        </div>
+        </div>  
     )
 }
