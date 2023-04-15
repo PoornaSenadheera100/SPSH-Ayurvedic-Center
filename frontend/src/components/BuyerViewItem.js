2c11a42a038af7a10fc231f3b48cef05db5a6be7
Sathira-Wijeratne <swije27@gmail.com>
2023-04-15 18:29:03 +0530

    added meaningful comments

--- frontend/src/components/BuyerViewItem.js
+++ frontend/src/components/BuyerViewItem.js
@@ -12,21 +12,24 @@ export default function BuyerViewItem() {
         window.location.replace("/buyerlogin");
     }
 
+    // States
     const [ProductId, setProductId] = useState("");
     const [SupplierId, setSupplierId] = useState("");
     const [Name, setName] = useState("");
     const [Description, setDescription] = useState("");
     const [Price, setPrice] = useState();
     const [Quantity, setQuantity] = useState();
-    const [MaxQuantity, setMaxQuantity] = useState();
-    const [Image, setImage] = useState("");
+    const [MaxQuantity, setMaxQuantity] = useState(); // Maximum quantity of the product available in stock
+    const [Image, setImage] = useState(""); // Base64 encoded string of the product image
+    const [rate, setRate] = useState(0); // Initial user rating for the product is set to 0
 
-    const [rate, setRate] = useState(0);
-
+    // Getting the product ID from the URL using useParams hook
     const { id } = useParams();
+    // Get the buyer's email from session storage
     const buyerEmail = sessionStorage.getItem("buyerEmail"); //implement this to get the buyer email from sessions
 
     useEffect(() => {
+        // Fetching the product data from the database
         axios.get(`http://localhost:8070/buyer/get/item/${id}`).then((res) => {
             console.log(res.data);
             console.log(buyerEmail);
@@ -42,13 +45,15 @@ export default function BuyerViewItem() {
             console.log(err);
         })
 
+        // Fetching the user rating for the product
         axios.get(`http://localhost:8071/rate/get/${buyerEmail}/${id}`).then((res)=>{
             console.log(res.data);
-            setRate(res.data[0].rate);
+            setRate(res.data[0].rate);// If user has already rated the product, set the rating to the user's previous rating
         })
 
     }, []);
 
+    // Function to add the product to the cart
     function add(e) {
         e.preventDefault();
 
@@ -79,24 +84,30 @@ export default function BuyerViewItem() {
         return imageSource;
     };
 
+    // Function to allow users to rate the product
     function rateProduct(value){
         const newRate = {
             id,
             buyerEmail,
             value
         };
+        // Check if the product has been rated before or not
         if (rate === 0){
+            // If not, add the rating to the server
             axios.post("http://localhost:8071/rate/add", newRate).catch((err)=>{
                 alert("Rating Service is not available.");
             });
         } else {
+             // If already rated, update the rating on the server
             axios.put("http://localhost:8071/rate/update", newRate).catch((err)=>{
                 alert("Rating Service is not available.");
             })
         }
     }
 
+    // Check if the product has an image
     if (Image !== "") {
+        // Return the product details with the image
         return (
             <div className="container">
                  <div style={{padding: '10px'}}><a type="button" href="/buyerhome" className="btn btn-secondary">Back</a></div>
@@ -118,12 +129,14 @@ export default function BuyerViewItem() {
                                         Price &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:  Rs.{parseFloat(Price).toFixed(2)}<br />
                                       
                                         Supplier : {SupplierId} <br/>
+                                        {/* Create a Rater component to allow users to rate the product */}
                                         <b>Rate the Product</b> &nbsp;
                                         <Rater onRate={(value)=>{
                                             rateProduct(value.rating);
                                         }} total={5} rating={rate} style={{ fontSize: '30px' }}/>
                                     </div>
                                 </div>
+                                {/* Create a button to add the product to the cart */}
                                 <button className="btn btn-success" style={{marginTop: '10px', width: '100%'}} onClick={(e) => { add(e); }}>Add to cart</button>
                             </form>
                         </div>
