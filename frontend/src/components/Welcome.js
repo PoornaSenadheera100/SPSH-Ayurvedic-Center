37cb917b70747cb0fab964aba6f1ef530546d7ea
SanuthiVihansa <subasofts@gmail.com>
2023-04-15 13:01:48 +0530

    Adding carts to the buttons

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -1,20 +1,30 @@
 export default function Welcome(){
+
     return(
         <div className="container">
             <center>
-            <div align="center" style={{border: '1px solid black', borderRadius: '5px', padding: '10px', maxWidth: '500px', margin: '0 auto'}}>
-                <h1>Welcome</h1>
-                <br></br>
-                <a href="/buyerlogin"><button className="btn btn-primary btn-lg">Buyer Login</button></a>
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                <a href="/sellerlogin"><button className="btn btn-warning btn-lg">Seller Login</button></a>
-                <br></br>
-                <br></br>
-                <br></br>
-                <p className="mb-0">Are you an administrator? <a href="/adminlogin" className="text-black-50 fw-bold">Log in here.</a></p>
-            </div>
+                <div align="center" style={{border: '1px solid black', borderRadius: '5px', padding: '10px', maxWidth: '500px', margin: '0 auto'}}>
+                    <h1>Welcome</h1>
+                    <br></br>
+                    <div style={{display: 'flex', justifyContent: 'center'}}>
+                        <a href="/buyerlogin" style={{textDecoration: 'none'}}>
+                        
+                            <div style={{backgroundColor: '#007bff', borderRadius: '5px', padding: '10px', height: '200px', width: '200px', margin: '0 10px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+                                <h3 style={{margin: '0', color: '#fff'}}>Buyer Login</h3>
+                            </div>
+                        </a>
+                        <a href="/sellerlogin" style={{textDecoration: 'none'}}>
+                            <div style={{backgroundColor: '#ffc107', borderRadius: '5px', padding: '10px', height: '200px', width: '200px', margin: '0 10px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+                                <h3 style={{margin: '0', color: '#fff'}}>Seller Login</h3>
+                            </div>
+                        </a>
+                    </div>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <p className="mb-0">Are you an administrator? <a href="/adminlogin" className="text-black-50 fw-bold">Log in here.</a></p>
+                </div>
             </center>
         </div>
-    
     )
 }
