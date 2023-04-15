2c1f463c10aa96ac6e54f83bf50a019c80926a79
SanuthiVihansa <subasofts@gmail.com>
2023-04-15 12:43:24 +0530

    commit border and heading css

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -2,7 +2,8 @@ export default function Welcome(){
     return(
         <div className="container">
             <center>
-                <h1>Welcome to SPSH Ayurvedic Center</h1>
+            <div align="center" style={{border: '1px solid black', borderRadius: '5px', padding: '10px', maxWidth: '500px', margin: '0 auto'}}>
+                <h1>Welcome</h1>
                 <br></br>
                 <a href="/buyerlogin"><button className="btn btn-primary btn-lg">Buyer Login</button></a>
                 &nbsp;&nbsp;&nbsp;&nbsp;
@@ -11,7 +12,9 @@ export default function Welcome(){
                 <br></br>
                 <br></br>
                 <p className="mb-0">Are you an administrator? <a href="/adminlogin" className="text-black-50 fw-bold">Log in here.</a></p>
+            </div>
             </center>
         </div>
+    
     )
 }
