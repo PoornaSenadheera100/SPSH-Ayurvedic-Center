897394fcff8b3d0bb2fa16e9c1377caa583f3034
SanuthiVihansa <subasofts@gmail.com>
2023-04-16 08:48:46 +0530

    import button bootstrap

--- frontend/src/components/SingleOrder.js
+++ frontend/src/components/SingleOrder.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
+import Button from 'react-bootstrap/Button';
 
 export default function SingleOrder(){
 
